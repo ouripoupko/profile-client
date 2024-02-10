d5805b8d8abf1388b879b57f0051dcdd50150035
ouripoupko <ouri.poupko@gmail.com>
2024-02-10 17:30:47 +0200

    fix agent address to dynamic address

--- src/app/agent.service.ts
+++ src/app/agent.service.ts
@@ -20,11 +20,11 @@ export class AgentService {
     private http: HttpClient) { }
 
   listen(server: string, identity: string, contract: string): EventSource {
-    return new EventSource(`${server}stream?agent=${identity}&contract=${contract}`);
+    return new EventSource(`${server}/stream?agent=${identity}&contract=${contract}`);
   }
 
   write(server: string, identity: string, contract: string, method: Method): Observable<any> {
-    const url = `${server}ibc/app/${identity}/${contract}/${method.name}`;
+    const url = `${server}/ibc/app/${identity}/${contract}/${method.name}`;
     let params = new HttpParams().set('action', 'contract_write');
     return this.http.post<any>(url, method, {...this.httpOptions, params: params}).pipe(
       tap(_ => console.log('wrote something')),
@@ -33,7 +33,7 @@ export class AgentService {
   }
 
   read(server: string, identity: string, contract: string, method: Method): Observable<any> {
-    const url = `${server}ibc/app/${identity}/${contract}/${method.name}`;
+    const url = `${server}/ibc/app/${identity}/${contract}/${method.name}`;
     let params = new HttpParams().set('action', 'contract_read');
     return this.http.post<any>(url, method, {...this.httpOptions, params: params}).pipe(
       tap(_ => console.log('read something')),
